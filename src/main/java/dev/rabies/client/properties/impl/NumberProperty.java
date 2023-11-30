732200f48f30ef7b98006a1c410058e9e477fb44
shitposts <66158890+RabiesDev@users.noreply.github.com>
2023-12-01 04:55:00 +0900

    update NumberProperty

--- src/main/java/dev/rabies/client/properties/impl/NumberProperty.java
+++ src/main/java/dev/rabies/client/properties/impl/NumberProperty.java
@@ -1,18 +1,21 @@
 package dev.rabies.client.properties.impl;
 
 import dev.rabies.client.properties.Property;
+import dev.rabies.client.properties.range.NumberRange;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.function.Supplier;
 
 @Getter
-public class NumberProperty<T extends Number> extends Property<T> {
+public class NumberProperty<T extends Comparable<T>> extends Property<T> {
+    private final NumberRange<T> range;
     private final T interval;
 
     @Builder
-    protected NumberProperty(String identifier, T defaultValue, T interval, Supplier<Boolean> dependency) {
+    protected NumberProperty(String identifier, T defaultValue, T interval, NumberRange<T> range, Supplier<Boolean> dependency) {
         super(identifier, defaultValue, dependency);
+        this.range = range;
         this.interval = interval;
     }
 }
